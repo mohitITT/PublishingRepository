e8167364c4e0e94b3f14af6282e5d841f6736281
Mohit Sadhwani <mohit.sadhwani@intimetec.com>
2022-03-09 10:41:15 +0530

    release 1.0.0

---
+++ AnimeSDK/animelibrary/src/main/java/com/intimetec/animelibrary/Anime.kt
@@ -0,0 +1,6 @@
+package com.intimetec.animelibrary
+
+class Anime {
+    private val animeDB = arrayListOf<String>("Naruto", "Dragon Ball Z", "Jujutsu Kaisen", "Beyblade", "Gintama", "Attack On Titan", "One Piece")
+    public fun generateAnime(): String = animeDB.random()
+}

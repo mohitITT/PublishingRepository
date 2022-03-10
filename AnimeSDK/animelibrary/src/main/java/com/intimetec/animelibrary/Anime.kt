8b4b9c75c959c25d91524b7e194ef7eb9efae501
MohitSadhwani <mohit.sadhwani@intimetec.com>
2022-03-10 11:33:20 +0530

    maven solution 1

--- AnimeSDK/animelibrary/src/main/java/com/intimetec/animelibrary/Anime.kt
+++ AnimeSDK/animelibrary/src/main/java/com/intimetec/animelibrary/Anime.kt
@@ -1,6 +1,15 @@
 package com.intimetec.animelibrary
 
 class Anime {
-    private val animeDB = arrayListOf<String>("Naruto", "Dragon Ball Z", "Jujutsu Kaisen", "Beyblade", "Gintama", "Attack On Titan", "One Piece")
+    private val animeDB = arrayListOf<String>(
+        "Naruto",
+        "Dragon Ball Z",
+        "Jujutsu Kaisen",
+        "Beyblade",
+        "Gintama",
+        "Attack On Titan",
+        "One Piece"
+    )
+
     public fun generateAnime(): String = animeDB.random()
 }
